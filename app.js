c0682d3c7995de4a8fb1a3aba0012cc329956240
PeriCode <perilin124@gmail.com>
2021-12-13 10:05:56 +0800

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const app = express();
+//
+console.log(process.env.API_KEY);
+const port = 3000;
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+//connect mongodb
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+//define userSchema: Mongoose Schema class object,依照mongoose官網 quick start撰寫
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+//Secret String Instead of Two Keys
+
+// Add plugins or middleware here. For example, middleware for hashing passwords
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+//define User Model:
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.listen(port, () => {
+  console.log(" connnected at 3000");
+});
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  //Save user data
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+//刪除所有的文檔:If Authentication Error happened, delete the old documents and register new users then login. It will work!
+// User.deleteMany({}, (err) => {
+//   console.log("successful delete");
+// });
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
