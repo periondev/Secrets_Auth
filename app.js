0bbbccf5cbb73f8e11bbd4f217020303dcaaa83b
PeriYumYum <perilin124@gmail.com>
2022-10-05 15:51:26 +0800

    Fix serialization and deserialization error

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 //jshint esversion:6
-require("dotenv").config();
-const express = require("express");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-
 const port = 3000;
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-app.use(express.static("public"));
-//view engine setup
-app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 //set session
 app.use(
   session({
-    secret: "Our little secret.",
-    resave: false,
+    secret: 'No one knows',
+    resave: true,
     saveUninitialized: false,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-//connect mongodb local
-mongoose.connect("mongodb://localhost:27017/userDB");
 
-//define userSchema: Mongoose Schema class object,依照mongoose官網 quick start撰寫
+//params of mongoose connection
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+//connect to mongodb atlas (connect your application) + app.listen
+mongoose.connect(process.env.CONNECT_DB, connectionParams, (err) => {
+  if (err) {
+    console.log('Connection failed.');
+  } else {
+    console.log('Database connected successfully.');
+    app.listen(port, () => console.log('Server is running on 3000'));
+  }
+});
+
+//define userSchema (mongoose quick start)
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
@@ -40,22 +52,24 @@ const userSchema = new mongoose.Schema({
   facebookId: String,
 });
 
-//add plugins or middleware here. For example, middleware for hashing passwords
+//add plugins and middleware for hashing passwords
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 //define User Model:
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 //passport use createStrategy (instead of LocalStrategy, according to passport-local-mongoose doc)
 passport.use(User.createStrategy());
 
 //passport 序列化與反序列化 (according to Passport doc)
 passport.serializeUser((user, done) => {
-  done(null, user);
+  done(null, user.id);
 });
-passport.deserializeUser((user, done) => {
-  done(null, user);
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
 });
 
 //use Google OAuth20 strategy
@@ -64,7 +78,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: 'http://localhost:3000/auth/google/secrets',
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log(`The Google ID is ${profile.id}`);
@@ -81,7 +95,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:3000/auth/facebook/secrets",
+      callbackURL: 'http://localhost:3000/auth/facebook/secrets',
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log(`The Facebook ID is ${profile.id}`);
@@ -92,109 +106,109 @@ passport.use(
   )
 );
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
 //Google auth route
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
 app.get(
-  "/auth/google/secrets",
-  passport.authenticate("google", { failureRedirect: "/login" }),
+  '/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    res.redirect("/secrets");
+    res.redirect('/secrets');
   }
 );
 
 //Facebook auth route
-app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get('/auth/facebook', passport.authenticate('facebook'));
 
 app.get(
-  "/auth/facebook/secrets",
-  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  '/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
   (req, res) => {
-    res.redirect("/secrets");
+    res.redirect('/secrets');
   }
 );
 
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-app.get("/register", (req, res) => {
-  res.render("register");
+//passport local register route
+app.post('/register', (req, res) => {
+  User.register({ username: req.body.username }, req.body.password, (err, user) => {
+    if (err) {
+      console.log('Error at register.', err);
+      res.redirect('/register');
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        console.log('successfully add new user.');
+        res.redirect('/secrets');
+      });
+    }
+  });
 });
 
-app.get("/logout", (req, res) => {
+//passport local auth route
+app.post('/login', (req, res) => {
+  const user = new User({ username: req.body.username, password: req.body.password });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secrets');
+      });
+    }
+  });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/logout', (req, res) => {
   req.logOut();
-  res.render("home");
+  res.render('home');
 });
 
-app.get("/secrets", (req, res) => {
+//read contents
+// secret: { $ne: null }
+app.get('/secrets', (req, res) => {
   User.find({ secret: { $ne: null } }, (err, foundUsers) => {
     if (err) {
       console.log(err);
     } else {
       if (foundUsers) {
-        res.render("secrets", { usersWithSecrets: foundUsers });
+        res.render('secrets', { usersWithSecrets: foundUsers });
       }
     }
   });
 });
-//Let users submit their secrets.
-app.get("/submit", (req, res) => {
+//read submit form if user pass auth
+app.get('/submit', (req, res) => {
   if (req.isAuthenticated) {
-    res.render("submit");
+    res.render('submit');
   } else {
-    res.redirect("/login");
+    res.redirect('/login');
   }
 });
 
-app.post("/register", (req, res) => {
-  User.register({ username: req.body.username }, req.body.password, (err, user) => {
-    if (err) {
-      console.log("Error at register.", err);
-      res.redirect("/register");
-    } else {
-      passport.authenticate("local")(req, res, () => {
-        console.log("successfully add new user.");
-        res.redirect("/secrets");
-      });
-    }
-  });
-});
-//passport local auth route fixing
-app.post("/login", (req, res) => {
-  const user = new User({ username: req.body.username, password: req.body.password });
-  req.login(user, (err) => {
-    if (err) {
-      console.log(err); //not working
-    } else {
-      passport.authenticate("local")(req, res, () => {
-        res.redirect("/secrets");
-      });
-    }
-  });
-});
-
-app.post("/submit", (req, res) => {
+//user submit content
+app.post('/submit', (req, res) => {
   const submittedSecret = req.body.secret;
   const userId = req.user.id;
-  //find db if userId exist, save secret message.
+
+  //find db if userId exist, save content
   User.findById(userId, (err, foundUser) => {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        foundUser.secret = submittedSecret;
-        foundUser.save(() => {
-          res.redirect("/secrets");
-        });
-      }
+      foundUser.secret = submittedSecret;
+      foundUser.save(() => {
+        res.redirect('/secrets');
+      });
     }
   });
-});
-
-app.listen(port, () => {
-  console.log("Listening on port 3000");
 });
